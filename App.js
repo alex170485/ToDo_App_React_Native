8fb7167e3573920e78a2717b5e88af04af73509b
alex170485 <alex.170485@icloud.com>
2021-03-20 12:36:03 +0300

    add input and button

--- App.js
+++ App.js
@@ -2,16 +2,24 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import {NavBar} from "./src/NavBar";
+import {AddTodo} from "./src/AddTodo";
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View >
       <NavBar title = {'Todo App'}/>
+        <View style={styles.container}>
+      <AddTodo/>
       <StatusBar style="auto" />
+        </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+    container: {
+        paddingHorizontal: 30,
+        paddingVertical: 20
+    }
 
 });
---
+++ src/AddTodo.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import {View, TextInput, Button, StyleSheet} from "react-native";
+
+
+
+
+export const AddTodo = props => {
+    return (
+        <View style = {styles.block}>
+            <TextInput style = {styles.input}/>
+            <Button title = 'Добавить'/>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    block: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    },
+    input: {
+        width: '70%',
+        padding: 10,
+        borderStyle: 'solid',
+        borderBottomWidth: 2,
+        borderBottomColor: '#3949ab'
+    }
+
+})
